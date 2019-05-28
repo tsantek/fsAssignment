81a21b4f040a8f4ecb6080ffaa6f29033eb3682e
tsantek <santek.vg@gmail.com>
2019-05-28 10:12:13 -0700

    done with first 3

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 var fs = require('fs');
+
+
+// // Challenge 1
+// fs.readFile('./challenge1/info.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         throw err;
+//     }
+//     console.log(data)
+// });
+
+
+
+// Challenge 2
+let names = fs.readFileSync('./challenge2/info.txt', 'utf-8')
+names = names + 'Name 1\n Name 2\n Name 3\n Name 4\n Name 5\n '
+fs.writeFileSync('./challenge2/info.txt', names)
+console.log(names)
+
+
+
+// // // Challenge 3
+// fs.rename('./challenge3/binfo.txt', './challenge3/info.txt', (err) => {
+//     if (err) throw err;
+//     console.log('Rename complete!');
+// });
